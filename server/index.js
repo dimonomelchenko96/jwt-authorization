5b8fae6efc559bdac6d0e41e8d6fe94c42c68e93
Dima <dimonomelchenko1@gmail.com>
2022-12-14 16:39:00 +0200

    start back

---
+++ server/index.js
@@ -0,0 +1,30 @@
+require('dotenv').config();
+
+const express = require('express');
+const cors = require('cors');
+const cookieParser = require('cookie-parser');
+const mongoose = require('mongoose');
+
+
+const PORT = process.env.PORT || 5000;
+const app = express();
+
+mongoose.set("strictQuery", false);
+
+app.use(express.json());
+app.use(cookieParser());
+app.use(cors());
+
+const start = async () => {
+    try {
+        await mongoose.connect(process.env.DB_URL, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+        })
+        app.listen(PORT, () => console.log(`Server start on PORT ${PORT}`))
+    } catch (e) {
+        console.log(e);
+    }
+}
+
+start();
