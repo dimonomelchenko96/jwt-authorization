c9b9c6c7fd6d6df6e6f9817ab2aa9f0544a8fa1b
Dima <dimonomelchenko1@gmail.com>
2022-12-20 15:40:47 +0200

    add controllers

---
+++ server/controllers/user-controller.js
@@ -0,0 +1,51 @@
+class UserController {
+    async registration (req, res, next) {
+        try {
+
+        } catch (e) {
+
+        }
+    }
+
+    async login (req, res, next) {
+        try {
+
+        } catch (e) {
+            
+        }
+    }
+
+    async logout (req, res, next) {
+        try {
+
+        } catch (e) {
+            
+        }
+    }
+
+    async activate (req, res, next) {
+        try {
+
+        } catch (e) {
+            
+        }
+    }
+    
+    async refresh (req, res, next) {
+        try {
+
+        } catch (e) {
+            
+        }
+    }
+
+    async getUser (req, res, next) {
+        try {
+            return res.json('server work')
+        } catch (e) {
+            
+        }
+    }
+}
+
+module.exports = new UserController();
--- server/index.js
+++ server/index.js
@@ -4,6 +4,7 @@ const express = require('express');
 const cors = require('cors');
 const cookieParser = require('cookie-parser');
 const mongoose = require('mongoose');
+const router = require('./router/index')
 
 
 const PORT = process.env.PORT || 5000;
@@ -14,6 +15,7 @@ mongoose.set("strictQuery", false);
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors());
+app.use('/api', router)
 
 const start = async () => {
     try {
